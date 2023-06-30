7e58f6c49034014b3079a31c141111d8bf836136
HARRY OMOLLOH <hthuku95@gmail.com>
2023-06-30 13:02:19 +0300

    Added csv

--- scripts/bitcoin.py
+++ scripts/bitcoin.py
@@ -5,6 +5,8 @@ import json
 from web3 import Web3
 import requests
 from dotenv import dotenv_values
+import csv
+
 
 env_vars = dotenv_values('.env')
 
@@ -46,28 +48,24 @@ def fetch_bitcoin_transactions(address, api_key):
     return []
 
 def calculate_bitcoin_volumes(transactions):
-    incoming_volume_usd = 0
-    outgoing_volume_usd = 0
+    incoming_transactions = []
+    outgoing_transactions = []
 
-    # Calculate the timestamp 60 days ago
     cutoff_date = int((datetime.now() - timedelta(days=60)).timestamp())
 
     for tx in transactions:
         outputs = tx.get('outputs', [])
         inputs = tx.get('inputs', [])
 
-        # Calculate the total output value for the current transaction (outgoing transactions)
         output_value = sum([int(output.get('value', 0)) / 10**8 for output in outputs])
-
-        # Calculate the total input value for the current transaction (incoming transactions)
         input_value = sum([int(input.get('output_value', 0)) / 10**8 for input in inputs])
 
-        timestamp_str = tx.get('received', '')  # Get the 'received' timestamp as a string
+        timestamp_str = tx.get('received', '')
 
         try:
             timestamp_formats = [
-                '%Y-%m-%dT%H:%M:%S.%fZ',  # Format with milliseconds and 'Z' at the end
-                '%Y-%m-%dT%H:%M:%S.%f'  # Format with milliseconds
+                '%Y-%m-%dT%H:%M:%S.%fZ',
+                '%Y-%m-%dT%H:%M:%S.%f'
             ]
 
             for timestamp_format in timestamp_formats:
@@ -76,16 +74,24 @@ def calculate_bitcoin_volumes(transactions):
                     timestamp = timestamp_dt.timestamp()
 
                     if timestamp >= cutoff_date:
-                        if output_value > 0:  # Outgoing transaction
+                        if output_value > 0:
                             usd_value = convert_to_usd(output_value, timestamp)
-                            print("USD Value",usd_value)
                             if usd_value is not None:
-                                outgoing_volume_usd += usd_value
-                        elif input_value > 0:  # Incoming transaction
+                                outgoing_transactions.append({
+                                    'asset_type': 'Bitcoin',
+                                    'transaction_hash': tx.get('hash'),
+                                    'timestamp': timestamp_dt.strftime('%d/%m/%Y'),
+                                    'value_usd': usd_value
+                                })
+                        elif input_value > 0:
                             usd_value = convert_to_usd(input_value, timestamp)
-                            print("USD Value",usd_value)
                             if usd_value is not None:
-                                incoming_volume_usd += usd_value
+                                incoming_transactions.append({
+                                    'asset_type': 'Bitcoin',
+                                    'transaction_hash': tx.get('hash'),
+                                    'timestamp': timestamp_dt.strftime('%d/%m/%Y'),
+                                    'value_usd': usd_value
+                                })
                         break
 
                 except ValueError:
@@ -97,7 +103,24 @@ def calculate_bitcoin_volumes(transactions):
         except ValueError:
             print(f"Invalid timestamp format for transaction: {tx}")
 
-    return incoming_volume_usd, outgoing_volume_usd
+    return incoming_transactions, outgoing_transactions
+
+def export_transaction_data_to_csv(incoming_transactions, outgoing_transactions):
+    filename = 'bitcoin_transactions.csv'
+
+    fieldnames = ['asset_type', 'transaction_hash', 'timestamp', 'value_usd']
+
+    with open(filename, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for transaction in incoming_transactions:
+            writer.writerow(transaction)
+
+        for transaction in outgoing_transactions:
+            writer.writerow(transaction)
+
+    print(f"Transaction data exported to {filename}")
 
 def convert_to_usd(amount, timestamp):
     # Check if conversion value is already cached
@@ -126,8 +149,10 @@ def convert_to_usd(amount, timestamp):
     
     return None
 
-bitcoin_one_transactions = fetch_bitcoin_transactions(btc_address_one,BITCOIN_EXPLORER_API_KEY)
-bitcoin_two_transactions = fetch_bitcoin_transactions(btc_address_two,BITCOIN_EXPLORER_API_KEY)
-calculate_bitcoin_volumes(bitcoin_one_transactions)
-calculate_bitcoin_volumes(bitcoin_two_transactions)
+# Example usage:
+bitcoin_one_transactions = fetch_bitcoin_transactions(btc_address_one, BITCOIN_EXPLORER_API_KEY)
+bitcoin_two_transactions = fetch_bitcoin_transactions(btc_address_two, BITCOIN_EXPLORER_API_KEY)
+incoming_transactions, outgoing_transactions = calculate_bitcoin_volumes(bitcoin_one_transactions)
+incoming_transactions2, outgoing_transactions2 = calculate_bitcoin_volumes(bitcoin_two_transactions)
+export_transaction_data_to_csv(incoming_transactions + incoming_transactions2, outgoing_transactions + outgoing_transactions2)
 
