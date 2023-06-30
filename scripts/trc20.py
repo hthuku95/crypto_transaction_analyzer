385638947750e82d4d8e643ce1c578708628d603
HARRY OMOLLOH <hthuku95@gmail.com>
2023-06-30 07:26:45 +0300

    Updated trc20.py

--- scripts/trc20.py
+++ scripts/trc20.py
@@ -1,15 +1,10 @@
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 from datetime import datetime, timedelta
-import json
-from web3 import Web3
 import requests
-
-COIN_API_KEY = "ec455fb4-aab6-44bf-88af-279555e4aaa9"
-CRYPTO_COMPARE_API_KEY = "42d127eac62b0177671afb0df449ed4cf7db75d5f9407af0e7d4724e86d1ee60"
 
 trc20_address = "TR9octGKwGi8EaBhphP8d8D6dTkSyMJKXW"
 TRONSCAN_API_KEY = "60003d7a-10cd-4d0c-9abe-5a781c1971c2"
+COIN_API_KEY = "ec455fb4-aab6-44bf-88af-279555e4aaa9"
+CRYPTO_COMPARE_API_KEY = "42d127eac62b0177671afb0df449ed4cf7db75d5f9407af0e7d4724e86d1ee60"
 
 def fetch_trc20_transactions(address, api_key):
     # TronGrid API configuration
@@ -40,52 +35,42 @@ def calculate_trc20_volumes(transactions):
     cutoff_date = int((datetime.now() - timedelta(days=60)).timestamp())
 
     for tx in transactions:
-        value_trx = float(tx['value']) / 10**6  # Convert value from sun to TRX
-        timestamp = int(tx['timestamp'])
+        contract_data = tx.get('raw_data', {}).get('contract', [{}])[0].get('parameter', {}).get('value', {})
+        if 'amount' not in contract_data:
+            continue
+
+        value_trx = float(contract_data['amount']) / 10**6  # Convert value from sun to TRX
+        timestamp = int(tx['block_timestamp']) / 1000  # Convert timestamp to seconds
 
         if timestamp >= cutoff_date:
             usd_value = convert_trx_to_usd(value_trx, timestamp)
             if usd_value is not None:
-                if trc20_address.lower() == tx['from'].lower():
+                if value_trx < 0:  # Outgoing transaction
                     outgoing_volume_usd += usd_value
-                if trc20_address.lower() == tx['to'].lower():
+                    print("Outgoing transaction from:", contract_data['owner_address'])
+                elif value_trx > 0:  # Incoming transaction
                     incoming_volume_usd += usd_value
+                    print("Incoming transaction to:", contract_data['to_address'])
 
     return incoming_volume_usd, outgoing_volume_usd
 
 def convert_trx_to_usd(value_trx, timestamp):
-    # Fetch TRX to USD exchange rate from CoinCap API at the timestamp
-    url = "https://api.coincap.io/v2/rates/tron/history"
-    params = {
-        'interval': 'm1',
-        'start': timestamp,
-        'end': timestamp + 60  # Add 60 seconds for 1-minute interval
-    }
-    headers = {
-        'Accepts': 'application/json',
-        'Authorization': f"Bearer {COIN_API_KEY}"
-    }
+    api_url = f"https://min-api.cryptocompare.com/data/pricehistorical?fsym=TRX&tsyms=USD&ts={timestamp}&api_key={CRYPTO_COMPARE_API_KEY}"
 
     try:
-        response = requests.get(url, params=params, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            if 'data' in data and len(data['data']) > 0:
-                price_usd = data['data'][0]['rateUsd']
-                value_usd = value_trx * float(price_usd)
-                return value_usd
-            else:
-                print("No price data available for TRON at the given timestamp")
-                return None
-        else:
-            print(f"Error fetching TRX to USD exchange rate. Status code: {response.status_code}")
-            return None
-    except Exception as e:
-        print(f"Exception occurred while fetching TRX to USD exchange rate: {str(e)}")
+        response = requests.get(api_url)
+        data = response.json()
+        usd_price = data['TRX']['USD']
+        usd_value = value_trx * usd_price
+        return usd_value
+    except requests.RequestException as e:
+        print("Error occurred while converting TRX to USD:", str(e))
         return None
-        
 
-trc20_transactions  = fetch_trc20_transactions(trc20_address,TRONSCAN_API_KEY)
-(a,b) = calculate_trc20_volumes(trc20_transactions)
-print("Incoming",a)
-print("Outgoing",b)
+trc20_transactions = fetch_trc20_transactions(trc20_address, TRONSCAN_API_KEY)
+print(trc20_transactions[-1])
+print(trc20_transactions[-2])
+print(trc20_transactions[-3])
+(a, b) = calculate_trc20_volumes(trc20_transactions)
+print("Incoming:", a)
+print("Outgoing:", b)
